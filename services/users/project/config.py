44209a84151d8f6f4209abefbb547231a37554c6
Alexander G. Fowler <alexfowler@gmail.com>
2019-10-06 21:51:18 +0800

    Add postgres docker db setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,22 +1,25 @@
 # services/users/project/config.py
 
+import os
+
 
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig:
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig:
     """Testing configuration"""
     TESTING = True
+    SQLACHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig:
     """Production configuration"""
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
