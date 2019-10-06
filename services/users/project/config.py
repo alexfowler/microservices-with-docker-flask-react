68bbcbc0eb5b6fe6daecd26a1ecfc846255f459a
Alexander G. Fowler <alexfowler@gmail.com>
2019-10-06 16:58:22 +0800

    Initial commit
    - Part 1, Chapter 5 of Microservices with Docker, Flask
    and React, a tutorial by testdriven.io

---
+++ services/users/project/config.py
@@ -0,0 +1,22 @@
+# services/users/project/config.py
+
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig:
+    """Development configuration"""
+    pass
+
+
+class TestingConfig:
+    """Testing configuration"""
+    TESTING = True
+
+
+class ProductionConfig:
+    """Production configuration"""
+    pass
+
